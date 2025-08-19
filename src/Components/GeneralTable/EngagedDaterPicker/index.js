02ec9d1582c6aa409c6f0415232c02ce7c1fc62e
Robert Kocharyan <robert@progress.local>
2025-08-19 12:13:42 +0400

    bugfix

--- src/Components/GeneralTable/EngagedDaterPicker/index.js
+++ src/Components/GeneralTable/EngagedDaterPicker/index.js
@@ -1,17 +1,25 @@
 import { DatePicker } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { postDate } from '../../../Services/generalTableService';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import { setIsUpdate } from '../../../Slices/generalDataSlice';
 
 const EngagedDaterPicker = () => {
   const date = useSelector((state) => state?.generalTableData?.date);
-  const [pickerDate, setPickerDate] = useState(date ? dayjs(date) : dayjs());
+  const [pickerDate, setPickerDate] = useState();
   const dispatch = useDispatch();
 
   
+  useEffect(() => {
+    if (date) {
+      setPickerDate(dayjs(date));
+    }
+  }, [date]);
+
+  
   const onOk = async(value) => {
+    setPickerDate(value)
     try {
       const res = await postDate(value) 
         dispatch(setIsUpdate())
